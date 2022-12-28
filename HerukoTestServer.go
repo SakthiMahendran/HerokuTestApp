46749a846437b18e67fd482e52cbec5c045b1a3a
SakthiMahendran <kingsakthi2005@gmail.com>
2022-12-28 09:37:30 +0530

    Improved the Server

--- HerukoTestServer.go
+++ HerukoTestServer.go
@@ -1,15 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-)
-
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Printf("r.Header.Get(\"User-Agent\"): %v\n", r.Header.Get("User-Agent"))
-		fmt.Fprintln(w, "Your using: ", r.Header.Get("User-Agent"))
-	})
+	ts := TestServer{}
 
-	http.ListenAndServe(":80", nil)
+	ts.StartServing()
 }
